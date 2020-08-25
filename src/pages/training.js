1ab14641165307e9119ac688c1733ccc394d1a29
Danger <51889207+DangerDan88@users.noreply.github.com>
2020-08-24 20:38:08 -0600

    fixing training page

--- src/pages/training.js
+++ src/pages/training.js
@@ -5,57 +5,57 @@ import SEO from "../components/seo"
 const trainingPage = () => (
   <Layout>
     <SEO title="Training" />
-    <div className="flex flex-col justify-center items-center bg-red-900">
-      <h1 className="text-white flex justify-center text-4xl">
-        2020 Price List
-      </h1>
-      <hr />
-      <div>
-        <h2 className="text-white text-3xl pt-12">Lessons</h2>
-        <hr />
-        <p className="text-white text-xl">45 Minute Lesson - $50</p>
-        <br />
-        <p className="text-white text-xl"> Group Jump Lesson - $45</p>
+    <div id="menu" className="container mx-auto px-4 lg:pt-24 lg:pb-64">
+      <div className="flex flex-wrap text-center justify-center">
+        <div className="w-full lg:w-6/12 px-4">
+          <h2 className="text-4xl font-semibold text-black">2020 Price List</h2>
+        </div>
       </div>
-      <div className="text-white text-3xl pt-12">
-        <h2>Training</h2>
-        <hr />
-        <p className="text-white text-xl">Single Training Ride - $50</p>
-        <br />
-        <p className="text-white text-xl">
-          Part Time Training (no lessons, 2 rides per week) - $300
-        </p>
-        <br />
-        <p className="text-white text-xl">
-          Full Time Training (no lessons, 4 rides per week) - $500
-        </p>
-        <br />
-        <p className="text-white text-xl">
-          Part Time Package (2 lessons per week, 2 training rides per month) -
-          $400
-        </p>
-        <br />
-        <p className="text-white text-xl">
-          Full Time Package (3 lessons per week, 4 training rides per month) -
-          $550
-        </p>
-      </div>
-      <div className="text-white text-3xl pt-12">
-        <h2>Other</h2>
-        <hr />
-        <p className="text-white text-xl">
-          Cross Country Schooling (group only) - $50
-        </p>
-        <p className="text-white text-xl pt-12">
-          Trailering - 50 cents per mile, min($40)
-        </p>
-        <p className="text-white text-xl pt-12">Show Coaching - $50 per day</p>
-        <p className="text-white text-xl pt-12 pb-32">
-          Buying or Selling Commission - 10% of purchase price{" "}
-        </p>
+      <div className="flex flex-wrap mt-12 justify-center bg-red-900 rounded-lg">
+        <div className="grid grid-cols-1 sm:grid-cols-6 md:grid-cols-6 lg:grid-cols-6 xl:grid-cols-6 gap-4">
+          <div className="col-span-2 sm:col-span-4 xl:col-span-4">
+            <h3 className="font-semibold text-black text-3xl">Lessons</h3>
+            <p className="text-2xl">Group Jump Lesson</p>
+            <p2 className="text-xl">$45</p2>
+          </div>
+          <div className="col-span-2 sm:col-span-1 xl:col-span-1 italic "></div>
+
+          <div className="col-span-2 sm:col-span-4 xl:col-span-4">
+            <h3 className="font-semibold text-black text-3xl">Training</h3>
+            <p className="text-2xl">Single Training Ride</p>
+            <p2 className="text-xl">$50</p2>
+            <p className="text-2xl">
+              Part Time Training (no lessons, 2 rides per week)
+            </p>
+            <p2 className="text-xl">$300</p2>
+            <p className="text-xl">
+              Full Time Training (no lessons, 4 rides per week)
+            </p>
+            <p2 className="text-xl">$500</p2>
+            <p className="text-2xl">
+              Part Time Package (2 lessons per week, 2 training rides per month)
+            </p>
+            <p2 className="text-xl">$400</p2>
+            <p className="text-2xl">
+              Full Time Package (3 lessons per week, 4 training rides per month)
+            </p>
+            <p2 className="text-xl">$550</p2>
+          </div>
+          <div className="col-span-2 sm:col-span-4 xl:col-span-4">
+            <h3 className="font-semibold text-black text-3xl">Other</h3>
+            <p className="text-2xl">Cross Country Schooling (group only)</p>
+            <p2 className="text-xl">$50</p2>
+            <p className="text-2xl">Trailering</p>
+            <p2 className="text-xl">50 cents per mile, min($40)</p2>
+            <p className="text-2xl">Show Coaching</p>
+            <p2 className="text-xl">$50</p2>
+            <p className="text-2xl">Buying or Selling Commission</p>
+            <p2 className="text-xl">10% of purchase price</p2>
+          </div>
+        </div>
       </div>
     </div>
   </Layout>
 )
-
+//look into tailwind multiple classes fix
 export default trainingPage
